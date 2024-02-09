ececf8aef9778f34dc5f96c0b11150779c98efb3
ag2478 <144286900+ag2478@users.noreply.github.com>
2024-02-09 17:51:13 -0500

    lab3

---
+++ LAB/Lab 3/stringplayer.java
@@ -0,0 +1,21 @@
+// **************************************************
+// StringPlay.java
+//
+// Play with String objects
+// **************************************************
+public class stringplay
+{
+public static void main (String[] args)
+{
+String college = new String ("PoDunk College");
+________________________________________________________; // part (a)
+int stringLength;
+String change1, change2, change3;
+________________________________________________________; // part (b)
+System.out.println (college + " contains " + stringLength + " characters.");
+changel = _____________________________________________; // part (c)
+change2 = _____________________________________________; // part (d)
+change3 = _____________________________________________; // part (e)
+System.out.println ("The final string is " + change3);
+}
+}
