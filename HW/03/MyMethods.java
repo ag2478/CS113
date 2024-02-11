21aeb09aa01567259d385efe0d9a58668ef7263c
ag2478 <144286900+ag2478@users.noreply.github.com>
2024-02-11 16:06:04 -0500

    die and my

--- HW/03/MyMethods.java
+++ HW/03/MyMethods.java
@@ -1,9 +1,18 @@
 
 public class MyMethods {
-    public static void main(String[] args){
-        System.out.println();
 
-        System.out.println("------------- End of Line -------------");
+    public double volume(double width, double length, double height){
+        
+        return width * length * height;
+
+    }
+
+    public double avgFaceValues(Die d1, Die d2){
+        
+        int fv1 = d1.roll();
+        int fv2 = d2.roll();
+        return (( fv1 + fv2 ) / 2); 
+
     }
 
 }
