b5315e761a08526248b3da737ea127831ff17981
ag2478 <144286900+ag2478@users.noreply.github.com>
2024-02-11 14:29:46 -0500

    book class

--- HW/03/Book.java
+++ HW/03/Book.java
@@ -1,9 +1,35 @@
 
 public class Book {
-    public static void main(String[] args){
-        System.out.println();
 
-        System.out.println("------------- End of Line -------------");
+    private String title;
+    private int pages;
+    
+    public Book(String title, int pages){
+    
+        this.title = "Java Programming";
+        this.pages = 200;
+
+    }
+
+    public String getTitle(){
+        
+    }
+
+    public int getPages(){
+
+    }
+
+    public String setTitle(){
+
+    }
+
+    public int setPages(){
+
+    }
+
+    public String toString(){
+
+        return "Book title: " + title + "\nTotal pages: " + pages + "\n\n------------- End of Line -------------";
     }
 
 }
