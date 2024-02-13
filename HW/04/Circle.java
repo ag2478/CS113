751730d0a9644bb89a4de98595b29c3c41dc342f
ag2478 <144286900+ag2478@users.noreply.github.com>
2024-02-12 19:35:04 -0500

    circle added

---
+++ HW/04/Circle.java
@@ -0,0 +1,55 @@
+
+
+public class Circle {
+    
+    private Point center;
+    private int radius;
+
+    public static void main(String[] args){
+        this.center = center;
+        this.radius = radius;
+
+    }
+
+    public int getRadius{
+
+        return radius;
+
+    }
+
+    public Point getCenter{
+
+        return center;
+
+    }
+
+    public void setCenter{
+
+        this.center = center;
+
+    }
+
+    public void setRadius{
+
+        this.radius = radius;
+
+    }
+    
+    public boolean equals(){
+
+        if (Point 1 == Point 2) 
+            return true;
+
+        else 
+            return false;
+    }
+
+    public Double circumference(){
+
+    }
+
+    public String toString(){
+
+    }
+
+}
