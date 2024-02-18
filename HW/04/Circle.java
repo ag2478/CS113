26c55a922ca8c6431a9aad88c66639a0d56b7bb1
ag2478 <144286900+ag2478@users.noreply.github.com>
2024-02-18 16:07:33 -0500

    4dine?

--- HW/04/Circle.java
+++ HW/04/Circle.java
@@ -5,13 +5,15 @@ public class Circle {
     private Point center;
     private double radius;
 
+    //i honestly still dont get private or the how/why
+
     public Circle(Point center, double radius){
         this.center = center;
         this.radius = radius;
 
     }
 
-    
+    //MAN is it roomy in here...
 
     public Double circumference(){
 
@@ -25,9 +27,11 @@ public class Circle {
 
     }
 
+    // if the compiler says it passes, then i got no problem wit it
+
     public String toString(){
 
-        return "This circle's center is X:" + center.x + " Y: " + center.y + "\nWith a Radius of: " + radius;
+        return "This circle's center is " + center + "\nWith a Radius of: " + radius;
 
     }
 
--- HW/04/CircleTest.java
+++ HW/04/CircleTest.java
@@ -11,12 +11,42 @@ public class CircleTest {
         int sp = random.nextInt(26) - 1;
         Point p1 = new Point(fp, sp);
         Point p2 = new Point(0,0);
+        //clearly an english major was not involved in the question writing process.
 
+        System.out.println("Please enter the radius of Circle 1: ");
+        double r1 = scanner.nextDouble();
+        double r2 = 5;
 
+        //organization is pointless here im jsut goin goff the prompt because my adhd holds me back from keeping track.
+        //you'd think this would be easier for us neuro-spicy people
 
-        System.out.println(fp + " " + sp );
-        System.out.println(p1 + " " + p2 );
+        Circle c1 = new Circle(p1,r1);
+        Circle c2 = new Circle(p2,r2);
 
+        double d1 = p1.distance(p2);
+
+        System.out.println("Circle 1: " + c1);
+        System.out.println("Circle 2: " + c2);
+        System.out.println();
+        //OH BOY HERE WE GOOOOO!!!!
+
+        if (c1.equals(c2)){
+
+            System.out.println("The distance between the two circles is: " + d1);
+
+        } else {
+
+            System.out.println("They ain't \"equal\"...\nSomething was supposed to happen here...");
+        }
+
+        System.out.println();
+        System.out.println();
+        System.out.println("------------- End of Line -------------");
+
+        //System.out.println(fp + " " + sp );
+        //System.out.println(p1);
+        //System.out.println(c2);
+        //remnants of my inability to understand if i was actually pulling this off before...
 
     }
 
