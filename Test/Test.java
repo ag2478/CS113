d36d6af950ea2e1c9fa99a96c06d57e6d2393b5d
ag2478 <144286900+ag2478@users.noreply.github.com>
2024-02-19 18:48:51 -0500

    class 5 i think

--- Test/Test.java
+++ Test/Test.java
@@ -1,9 +1,35 @@
 package CS113.Test;
-
+import java.util.*;
 public class Test
 {
     public static void main(String[] args)
     {
-        System.out.println("another test run");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("enter a number: ");
+        int entry = scanner.nextInt();
+
+
+
+        switch(entry){
+
+            case 1 :
+            System.out.println(1);
+            break;
+
+            case 2 :
+            System.out.println(2);
+            break;
+
+            case 3 :
+            System.out.println(3);
+            break;
+
+            default :
+            System.out.println("Error");
+            break;
+    }
+
+       System.out.println("another test run");
+
     }
 }
